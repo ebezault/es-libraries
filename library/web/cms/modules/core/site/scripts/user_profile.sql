1deb9aa3e890381c7f40d1241fbd2e40e1f44d0e
Jocelyn Fiat <github@djoce.net>
2025-09-16 14:00:04 +0200

    Updated user_profile sql schema

--- library/web/cms/modules/core/site/scripts/user_profile.sql
+++ library/web/cms/modules/core/site/scripts/user_profile.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user_profiles(
-  `uid` INTEGER NOT NULL CHECK("uid">=0),
+  `uid` INTEGER NOT NULL,
   `key` VARCHAR(255) NOT NULL,
   `value` TEXT,
   CONSTRAINT PK_uid_key PRIMARY KEY (`uid`,`key`)
